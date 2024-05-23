411ed3f500996d72957d74eed161d70bcf391eb8
Adven-True <154115104+Adven-True@users.noreply.github.com>
2024-05-23 18:03:14 -0400

    5.9 third commit

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/api/ApiController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/api/ApiController.java
@@ -42,13 +42,13 @@ public class ApiController {
 	@Autowired
 	private ScheduleService scheduleService;
 
-	//删除排班接口
+
 	@PostMapping("schedule/remove")
 	public Result removeSchedule(HttpServletRequest request){
-		//获取到传递过来排班的信息
+
 		Map<String, String[]> requestMap = request.getParameterMap();
 		Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
-		//获取医院编号和排班编号
+
 		String hoscode = verifySignCode(paramMap);
 		String hosScheduleId = (String) paramMap.get("hosScheduleId");
 
@@ -57,17 +57,17 @@ public class ApiController {
 
 	}
 
-	//查询医院排班的接口
+
 	@PostMapping("schedule/list")
 	public Result findSchedule(HttpServletRequest request){
-		//获取到传递过来的信息
+
 		Map<String, String[]> requestMap = request.getParameterMap();
 		Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
 
-		//获取当前页和每页记录数
+
 		int page = StringUtils.isEmpty(paramMap.get("page")) ? 1 : Integer.parseInt((String) paramMap.get("page"));
 		int limit = StringUtils.isEmpty(paramMap.get("limit")) ? 1 : Integer.parseInt((String) paramMap.get("limit"));
-		//签名校验
+
 		String hoscode = verifySignCode(paramMap);
 		String depcode = (String) paramMap.get("depcode");
 		ScheduleQueryVo scheduleQueryVo = new ScheduleQueryVo();
@@ -77,13 +77,13 @@ public class ApiController {
 		return Result.ok(schedulesModel);
 	}
 
-	//上传排班的接口
+
 	@PostMapping("saveSchedule")
 	public Result saveSchedule(HttpServletRequest request){
-		//获取到传递过来的排班信息
+
 		Map<String, String[]> requestMap = request.getParameterMap();
 		Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
-		//签名校验
+
 		verifySignCode(paramMap);
 
 		scheduleService.save(paramMap);
@@ -91,37 +91,37 @@ public class ApiController {
 		return Result.ok();
 	}
 
-	//删除科室接口
+
 	@PostMapping("department/remove")
 	public Result removeDepartment(HttpServletRequest request){
-		//获取到传递过来的科室信息
+
 		Map<String, String[]> requestMap = request.getParameterMap();
 		Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
 
-		//获取医院的编号
+
 		String hoscode = (String) paramMap.get("hoscode");
-		//获取科室编号
+
 		String depcode = (String) paramMap.get("depcode");
-		//签名校验
+
 		verifySignCode(paramMap);
-		//删除科室接口
+
 		departmentService.remove(hoscode,depcode);
 
 		return Result.ok();
 	}
 
-	//查询科室接口
+
 	@PostMapping("department/list")
 	public Result findDepartment(HttpServletRequest request){
-		//获取到传递过来的科室信息
+
 		Map<String, String[]> requestMap = request.getParameterMap();
 		Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
-		//获取医院的编号
+
 //		String hoscode = (String) paramMap.get("hoscode");
-		//获取当前页和每页记录数
+
 		int page = StringUtils.isEmpty(paramMap.get("page")) ? 1 : Integer.parseInt((String) paramMap.get("page"));
 		int limit = StringUtils.isEmpty(paramMap.get("limit")) ? 1 : Integer.parseInt((String) paramMap.get("limit"));
-		//签名校验
+
 		String hoscode = verifySignCode(paramMap);
 
 		DepartmentQueryVo departmentQueryVo = new DepartmentQueryVo();
@@ -132,64 +132,62 @@ public class ApiController {
 		return Result.ok(pageModel);
 	}
 
-	//上传科室接口
+
 	@PostMapping("saveDepartment")
 	public Result saveDepartment(HttpServletRequest request){
-		//获取到传递过来的科室信息
+
 		Map<String, String[]> requestMap = request.getParameterMap();
 		Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
 		verifySignCode(paramMap);
-		//调用service里面的方法
+
 		departmentService.save(paramMap);
 		return Result.ok();
 	}
 
 
-	//查询医院的接口
+
 	@PostMapping("hospital/show")
 	public Result getHospital(HttpServletRequest request){
-		//获取到传递过来的医院信息
+
 		Map<String, String[]> requestMap = request.getParameterMap();
 		Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
 
-		//获取request的信息返回hosCode
+
 		String hosCode = verifySignCode(paramMap);
 
-		//调用service方法实现根据医院编号查询
+
 		Hospital hospital = hospitalService.getByHoscode(hosCode);
 		return Result.ok(hospital);
 	}
 
-	//验证签名，返回hosCode
+
 	private String verifySignCode(Map<String, Object> paramMap) {
 
-		//获取传递过来的医院编号
+
 		String hosCode = (String) paramMap.get("hoscode");
-		//获取医院系统传递过来的签名,签名是进行MD5的加密
+
 		String hospSign = (String) paramMap.get("sign");
-		//根据传递过来的医院编码，查询数据库，查询签名
+
 		String signKey = hospitalSetService.getSignKey(hosCode);
-//		//把数据库查询签名进行MD5加密
-//		String signKeyMD5 = MD5.encrypt(signKey);
-		//判断签名是否一致
+
 		if (!hospSign.equals(signKey)){
 			throw new YyghException(ResultCodeEnum.SIGN_ERROR);
 		}
 		return hosCode;
 	}
 
-	//上传医院接口
+
 	@PostMapping("saveHospital")
 	public Result saveHosp(HttpServletRequest request){
-		//获取到传递过来的医院的信息
+
 		Map<String, String[]> requestMap = request.getParameterMap();
 		Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
 		verifySignCode(paramMap);
-		//传输过程中“+”转换成了“ ”,因此我们需要转换过来
+
 		String logoData = (String) paramMap.get("logoData");
 		logoData = logoData.replaceAll(" ","+");
 		paramMap.put("logoData",logoData);
-		//调用service中的方法
+
 		hospitalService.save(paramMap);
 		return Result.ok();
 	}
